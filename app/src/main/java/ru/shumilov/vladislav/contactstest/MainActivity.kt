f28f20bf1d6b2111fb0d905946e7c3fff6fde6b9
shumilov <shumilov@simpls.ru>
2019-07-04 22:19:58 +0500

    first commit

---
+++ app/src/main/java/ru/shumilov/vladislav/contactstest/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.shumilov.vladislav.contactstest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
